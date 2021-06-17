945829cdcf71ac0239ff97b70996dd8dde8a4eb7
prodigos <wilghensantos@gmail.com>
2021-06-17 19:41:33 -0400

    add new function to push message to array

--- script.js
+++ script.js
@@ -3,35 +3,53 @@ const randomNumber = (num) => {
     return Math.floor(Math.random() * num)
 }
 
-const callAvengers = {
-    "hero": ["Cap. America", "Ironman", "HawkEye", "Hulk","Ant-man"],
-    "villain": ["Thanos", "Loki", "Ultron", "Hella","Warmonger"],
-    "place": ["Wakanda", "Slovenia", "Asgard", "New York" ]
+// const callAvengers = {
+//     "hero": ["Cap. America", "Ironman", "HawkEye", "Hulk","Ant-man"],
+//     "villain": ["Thanos", "Loki", "Ultron", "Hella","Warmonger"],
+//     "place": ["Wakanda", "Slovenia", "Asgard", "New York" ]
 
+// }
+// let summitAvengers = [];
+
+// for (let key in callAvengers) {
+//     // console.log(key);
+//     // console.log(callAvengers[key])
+//     // console.log(randomNumber(callAvengers[key].length))
+//     let randomIndex = randomNumber(callAvengers[key].length);
+//     switch(key) {
+//         case "hero" : 
+//         summitAvengers.push(`We need ${callAvengers[key][randomIndex]} to help us.`)
+//         break;
+//         case "villain":
+//         summitAvengers.push(`${callAvengers[key][randomIndex]} is on the loose,`)
+//         break;
+//         case "place": 
+//         summitAvengers.push(`and the villain is terrorising ${callAvengers[key][randomIndex]}`)
+//         break;
+//         default:
+//         summitAvengers.push("we are all doomed")
+//      } 
+// } 
+
+// const emergency = (arr) => {
+//     // arr.join()
+//     console.log(arr.join(' '))
+// }
+// emergency(summitAvengers)
+
+let heroes = ["Cap. America", "Ironman", "HawkEye", "Hulk","Ant-man"];
+let villains = ["Thanos", "Loki", "Ultron", "Hella","Warmonger"];
+let places = ["Wakanda", "Slovenia", "Asgard", "New York"];
+
+let message = [];
+
+const summit = (arr) => {
+    for (i = 0; i < arr.length; i++){
+        let random = randomNumber(arr.length);
+        message.push(arr[random])
+    }
+    return message
 }
-let summitAvengers = [];
 
-for (let key in callAvengers) {
-    // console.log(key);
-    // console.log(callAvengers[key])
-    // console.log(randomNumber(callAvengers[key].length))
-    let randomIndex = randomNumber(callAvengers[key].length);
-    switch(key) {
-        case "hero" : 
-        summitAvengers.push(callAvengers[key][randomIndex])
-        break;
-        case "villain":
-        summitAvengers.push(callAvengers[key][randomIndex])
-        break;
-        case "place": 
-        summitAvengers.push(callAvengers[key][randomIndex])
-        break;
-        default:
-            summitAvengers.push("we are all doomed")
-     } 
-} 
 
-const emergency = (arr) => {
-    console.log(arr)
-}
-emergency(summitAvengers)
+console.log(summit(heroes))
